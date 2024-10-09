2182c4fd1c62bd866fecdddc6720db3235498379
Mennah <mennazqqq@gmail.com>
2024-10-09 13:05:17 +0200

    services implementation

--- src/Application/Interfaces/IPatientService.cs
+++ src/Application/Interfaces/IPatientService.cs
@@ -5,5 +5,10 @@ namespace Application.Interfaces
     public interface IPatientService
     {
         bool AddNewPatient(Patient patient);
+        bool RemovePatient(uint id);
+        bool UpdatePatient(Patient patient);
+        Patient GetPatient(uint id);
+        IEnumerable<Patient> GetAllPatients(Patient patient);
     }
+
 }
--- src/Application/Services/PatientService.cs
+++ src/Application/Services/PatientService.cs
@@ -2,6 +2,7 @@
 using DataAccess.Entities;
 using DataAccess.Interfaces;
 
+
 namespace Application.Services
 {
     public class PatientService : IPatientService
@@ -15,6 +16,30 @@ namespace Application.Services
         public bool AddNewPatient(Patient patient)
         {
             repository.Create(patient);
+            return true;
+        }
+
+        public IEnumerable<Patient> GetAllPatients(Patient patient)
+        {
+            return repository.GetAll().ToList();
+        }
+
+        public Patient GetPatient(uint id)
+        {
+            return repository.GetById((int)id);
+        }
+
+        public bool RemovePatient(uint id)
+        {
+            var p = repository.GetById((int)id);
+            repository.Delete(p);
+            return true;
+        }
+
+        public bool UpdatePatient(Patient patient)
+        {
+            var p = repository.GetById((int)patient.id);
+            repository.Update(patient);
             return true;
         }
     }
